16b4d81f5d92745a443b5672cf6654b8270e3e84
hillaryzinhaa172 <hillary.loiola@escola.pr.gov.br>
2025-08-21 12:00:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual a raiz quadrada de 121?',
+    '11'
+)
+
+criaCartao(
+    'História',
+    'Em que ano aconteceu a Segunda Guerra Mundial?',
+    '1939 a 1945'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
